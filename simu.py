a442775ab81dbed151f2b6d09caf8d369237540b
adibou <adib.elansari@epfl.ch>
2026-07-29 17:00:08 -0400

    jpppppppp

--- simu.py
+++ simu.py
@@ -75,8 +75,8 @@ class config:
                 simu_title="simu_"
             if tfin is None : 
                 if term=='short' : tfin=400
-                elif term=='mid' : tfin=30000
-                elif term=='long' : tfin=30000
+                elif term=='mid' : tfin=20000
+                elif term=='long' : tfin=20000
                 else:tfin=40
 
             if run_index is not None:
